c052c8c7c900de8a9de72ab03fdbdbfa9e503887
Smurf <sudaru2@gmail.com>
2019-10-26 21:42:15 +0300

    add new flags for KLPartitioner

--- KLPartitoner.go
+++ KLPartitoner.go
@@ -1,23 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"math"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
+	boolmatrixlib "github.com/Rakiiii/goBoolMatrix"
 	graphlib "github.com/Rakiiii/goGraph"
 	klpartitionlib "github.com/Rakiiii/goKLPartition"
 )
 
+var res = "res"
+
 func main() {
+	//flag for first time saving
+	ftimeFlag := true
+
+	//condition flag to start next itteration
 	itterationFlag := true
+
+	//saving type of flag
 	typeFlag := ""
+
+	//er init
 	var er error
+
+	//flag for reload testing
+	rFlag := false
+
+	//path to graph
+	graphPath := os.Args[2]
+
+	//parse graph
+	var parser = new(graphlib.Parser)
+	var g, err = parser.ParseUnweightedUndirectedGraphFromFile(graphPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//itteration stop conditions
 	maxItteration := 0
 	maxTime, _ := time.ParseDuration("0ms")
+
+	//var for result of algorithm
+	result := klpartitionlib.Result{Matrix: nil, Value: -1}
 
 	if er != nil {
 		log.Panicln(er)
@@ -38,19 +70,31 @@ func main() {
 			log.Println(er)
 			return
 		}
+	case "-r":
+		typeFlag = "-i"
+		maxItteration, er = strconv.Atoi(os.Args[3])
+		if er != nil {
+			log.Panicln(er)
+		}
+		r, err := parseResult(g.AmountOfVertex())
+		if err != nil {
+			if os.IsNotExist(err) {
+				rFlag = true
+			} else {
+				log.Panicln(err)
+				return
+			}
+		} else {
+			result.Matrix = r.Matrix
+			result.Value = r.Value
+		}
+	case "-mi":
+		typeFlag = "-i"
+		maxItteration = -1
+		ftimeFlag = false
 	default:
 		log.Panicln("Wrong input flag")
 	}
-	graphPath := os.Args[2]
-
-	var parser = new(graphlib.Parser)
-	var g, err = parser.ParseUnweightedUndirectedGraphFromFile(graphPath)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	//var for result of algorithm
-	result := klpartitionlib.Result{Matrix: nil, Value: -1}
 
 	//counte time
 	var prevValue int64 = math.MaxInt64
@@ -67,12 +111,14 @@ func main() {
 		//partition of graph
 		result, err = klpartitionlib.KLPartitionigAlgorithm(g, result.Matrix)
 
+		fmt.Println("Value:", result.Value)
+
 		//getting time after algoritm stops
 		endTime = time.Now()
 		//fmt.Println(endTime.Nanosecond())
 
 		//init time of fitst itteration if needs
-		if firstItTime.Nanoseconds() == 0 {
+		if firstItTime.Nanoseconds() == 0 && ftimeFlag {
 			firstItTime = endTime.Sub(startTime)
 			//fmt.Println("first duration:", firstItTime.Nanoseconds())
 		}
@@ -83,16 +129,22 @@ func main() {
 
 		//checked for local max founding
 		if prevValue <= result.Value {
+			firstItTime = endTime.Sub(startTime)
 			writeTime(firstItTime)
-			saveResult(&result)
+			saveResult(&result, "result_"+os.Args[2])
 			return
 		}
 
 		if endTime.Sub(startTime).Milliseconds() > maxTime.Milliseconds() && typeFlag == "-t" {
 			itterationFlag = false
 		}
-		if itterationNumber >= maxItteration && typeFlag == "-i" {
+		if itterationNumber >= maxItteration && typeFlag == "-i" && maxItteration != -1 {
 			itterationFlag = false
+		}
+
+		if rFlag {
+			rFlag = false
+			saveResult(&result, res)
 		}
 
 		itterationNumber++
@@ -100,7 +152,7 @@ func main() {
 
 	//save result and time
 	writeTime(firstItTime)
-	saveResult(&result)
+	saveResult(&result, "result_"+os.Args[2])
 
 }
 
@@ -115,8 +167,8 @@ func writeTime(time time.Duration) {
 	}
 }
 
-func saveResult(result *klpartitionlib.Result) {
-	f, err := os.Create("result_" + os.Args[2])
+func saveResult(result *klpartitionlib.Result, path string) {
+	f, err := os.Create(path)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println(result.Value)
@@ -145,4 +197,50 @@ func saveResult(result *klpartitionlib.Result) {
 
 	}
 
+}
+
+func parseResult(length int) (klpartitionlib.Result, error) {
+	file, err := os.Open(res)
+	if os.IsNotExist(err) {
+		return klpartitionlib.Result{Value: -1, Matrix: nil}, err
+	}
+	defer file.Close()
+	res := klpartitionlib.Result{Matrix: nil, Value: -1}
+	var bm boolmatrixlib.BoolMatrix
+	bm.Init(2, length)
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	subVal, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		return klpartitionlib.Result{Value: -1, Matrix: nil}, err
+	} else {
+		res.Value = int64(subVal)
+	}
+
+	it := 0
+	for scanner.Scan() {
+		parts := strings.Fields(scanner.Text())
+		fr, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return klpartitionlib.Result{Value: -1, Matrix: nil}, err
+		}
+		if fr == 0 {
+			bm.SetBool(it, 0, false)
+		} else {
+			bm.SetBool(it, 0, true)
+		}
+		sr, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return klpartitionlib.Result{Value: -1, Matrix: nil}, err
+		}
+		if sr == 0 {
+			bm.SetBool(it, 1, false)
+		} else {
+			bm.SetBool(it, 1, true)
+		}
+		it++
+	}
+	res.Matrix = &bm
+	return res, nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Rakiiii/goKLPartitoner
 go 1.13
 
 require (
-	github.com/Rakiiii/goGraph v0.1.0
-	github.com/Rakiiii/goKLPartition v0.1.1
+	github.com/Rakiiii/goBoolMatrix v0.1.2
+	github.com/Rakiiii/goGraph v0.1.1
+	github.com/Rakiiii/goKLPartition v0.1.4
 )
